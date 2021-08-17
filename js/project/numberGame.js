e50dd9ff3d17bdf603d7b6fd4c54b3ec483335a4
cmpmmq <chen.mengqi7@qq.com>
2021-08-17 15:44:15 +0800

    Site updated: 2021-08-17 15:44:15

--- js/project/numberGame.js
+++ js/project/numberGame.js
@@ -1,8 +1,25 @@
 // 随机函数产生答案
-let kString = '1233';
+function getRandom(n){
+    let kNum;
+    do{
+        kNum = parseInt(Math.random()*Math.pow(10,n));
+    }
+    while(kNum < Math.pow(10, n-1));
+
+    return kNum.toString();
+}
 
 // 设置游戏难度和猜数字位数
+// btn onclick
+function gameGenerate(n){
+    n = Number(document.getElementById('n').value);
+    kString = getRandom(n);
+    // document.getElementById('kString').value = kString;
+    // document.getElementById('gameDisplay').innerHTML = '答案已生成，游戏开始';
+    console.log(kString);
+}
 
+// btn onclick
 function gameCaculate(n, iString){
     // 计分
     let result = [0,0];
@@ -48,17 +65,22 @@ function gameCaculate(n, iString){
 
 let sum = 0;
 
-function clickButton(){
+function guessNumber(){
     sum ++;
-    let iNum = document.getElementById('iNum').value;
-    kString = document.getElementById('kString').value;
-    const n = kString.length;
+    let iString = document.getElementById('iNum').value;
+    // kString = document.getElementById('kString').value;
+    const n = Number(document.getElementById('n').value);
+    
 
-    let result = gameCaculate(n, iNum);
+    let result = gameCaculate(n, iString);
     let newRow = document.getElementById('gameBoard').insertRow(sum);
     newRow.insertCell(0).innerHTML = sum;
-    newRow.insertCell(1).innerHTML = iNum;
+    newRow.insertCell(1).innerHTML = iString;
     newRow.insertCell(2).innerHTML = result[0];
     newRow.insertCell(3).innerHTML = result[1];
-    newRow.insertCell(4).innerHTML = kString;
+    // newRow.insertCell(4).innerHTML = kString;
+
+    if(kString == iString){
+        document.getElementById("gameResult").innerHTML = "恭喜你猜对了";
+    }
 }

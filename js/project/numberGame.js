a5c6ddbab0b263424024ffc6fa560021a1f04bc1
cmpmmq <chen.mengqi7@qq.com>
2021-08-21 01:52:22 +0800

    Site updated: 2021-08-21 01:52:22

--- js/project/numberGame.js
+++ js/project/numberGame.js
@@ -1,9 +1,45 @@
 let times = 0;      // 游戏盘数
 
-// 产生n位随机数
-function getRandom(n){
+// object and arrays
+const newGame = {};
+let eachGames = [
+    {
+        id: -1,
+        n: 3,
+        isRepeat: false,
+        kString: '493',
+        eachGuesses: [
+            {
+                iString: '123',
+                result: [1, 1]
+            }
+        ],
+        time: "/样例数据"
+    },
+    {
+        id: 0,
+        n: 4,
+        isRepeat: true,
+        kString: '3173',
+        eachGuesses: [
+            {
+                iString: '1234',
+                result: [0, 2]
+            }
+        ],
+        time: "/样例数据"
+    }
+];
+
+
+// localStorage.setItem("eachGames", JSON.stringify(eachGames));
+// eachGames = localStorage.getItem("eachGames");
+
+
+// 产生n位isRepeat随机数
+function getRandom(n, isRepeat){
     let kNum, kString = new Array();
-    if(document.getElementById("isRepeatY").checked){
+    if(isRepeat){
         do{
             kNum = parseInt(Math.random()*Math.pow(10,n));
         }
@@ -27,42 +63,35 @@ function getRandom(n){
 }
 
 // 设置游戏难度和猜数字位数
-// btn onclick
-function gameGenerate(){
+function gameGenerateClick(){
     if(document.getElementById("n").reportValidity()){
-        const n = Number(document.getElementById('n').value);
-        kString = getRandom(n);     // 产生n位数的答案
-
-        // 是否内测
-        // document.getElementById('kString').value = kString;
-        // document.getElementById('gameDisplay').innerHTML = '答案已生成，游戏开始';
-        
+        // 读取并统一保存数据
+        newGame.id = eachGames.length - 1;
+        newGame.n = Number(document.getElementById('n').value);
+        newGame.isRepeat = document.getElementById("isRepeatY").checked;
+        newGame.kString = getRandom(newGame.n, newGame.isRepeat);
+        newGame.eachGuesses = [];
+        const {n, isRepeat, kString} = newGame;
         console.log(kString);       // 后台答案
 
-        let isRepeat;
-        if(document.getElementById("isRepeatY").checked){
-            isRepeat = "可能有";
-        } else{
-            isRepeat = "一定无";
-        }
-        
         // 游戏界面调整
         document.getElementById("guessBoard").style.display = "block";   // 显示猜数字板
         document.getElementById("answerBoard").style.display = "none";  // 禁用难度板
-        document.getElementById("levelDisplay").innerHTML = `第 ${times+1} 局：已成功加载游戏难度为 ${n} 位数、${isRepeat}重复数字 的游戏，开始挑战！`;
-        document.getElementById("newGame").value = "重新开始";
+        document.getElementById("guessDisplay").style.display = "block";    // btns
+        document.getElementById("times").innerHTML = eachGames.length - 1;
+        document.getElementById("nDisplay").innerHTML = n;
+        const isRepeatY = isRepeat? "可能有" : "一定无";
+        document.getElementById("isRepeatDisplay").innerHTML = isRepeatY;
 
         // 限定游戏板 输入的数字范围
         document.getElementById("iString").min = Math.pow(10, n-1);
         document.getElementById("iString").max = Math.pow(10, n)-1;
-    } else{
-        document.getElementById("n").value = 3;
-    }
+    } else{ document.getElementById("n").value = 3;}
 }
 
-// 游戏玩耍的计分
+// n位iString输入，游戏玩耍的计分，得到result(2)
 function gameCaculate(n, iString){
-    // 计分
+    const {kString} = newGame;
     let result = [0,0];
 
     const keyNum = new Array(n);
@@ -94,68 +123,86 @@ function gameCaculate(n, iString){
         }
     }
     
-    //  算法的测试 后台运行
-        // console.log(keyNum);
-        // console.log(inputNum);
-        // console.log(bookKey);
-        // console.log(bookInput);
-
     return result;
 }
 
-let sum = 0;
-
-// 获得游戏输入
-// btn onclick
-function guessNumber(){
-    const n = Number(document.getElementById('n').value);
-    const iString = document.getElementById('iString').value;
-    // kString = document.getElementById('kString').value;
-    
-    const isRepeat = document.getElementById("isRepeatY").checked.toString();      // 提交答案难度
-
-    // 合法输入
-    if(document.getElementById("iString").reportValidity()){
-        sum ++;
-        let result = gameCaculate(n, iString);      // 获得游戏计分反馈
-        let newRow = document.getElementById('gameBoard').insertRow(sum);
-        newRow.insertCell(0).innerHTML = sum;
-        newRow.insertCell(1).innerHTML = iString;
-        newRow.insertCell(2).innerHTML = result[0];
-        newRow.insertCell(3).innerHTML = result[1];
-        // newRow.insertCell(4).innerHTML = kString;
-
-        if(kString == iString){
-            document.getElementById("gameResult").innerHTML = "恭喜你猜对了！";
-            document.getElementById("newGame").value = "再来一局"
-            times ++;
-
-            document.getElementById("scoreBoard").style.display = "block";
-            let newScore = document.getElementById('scoreBoard').insertRow(times);
-            newScore.insertCell(0).innerHTML = times;
-            newScore.insertCell(1).innerHTML = n;
-            newScore.insertCell(2).innerHTML = isRepeat.toString();
-            newScore.insertCell(3).innerHTML = kString;
-            newScore.insertCell(4).innerHTML = sum;
-            newScore.insertCell(5).innerHTML = "...";
-        }
-    }
-    
-    document.getElementById("iString").value = "";
-    // document.getElementById("iString").placeholder = iString;
+// 写入表格该局eachGames[].eachGuesses 第n次数据，到第n行
+function tableWriteGame(eachGuesses, n){
+    let newRow = document.getElementById('gameDisplay').insertRow(n);
+    newRow.insertCell(0).innerHTML = n;
+    newRow.insertCell(1).innerHTML = eachGuesses[n-1].iString;
+    newRow.insertCell(2).innerHTML = eachGuesses[n-1].result[0];
+    newRow.insertCell(3).innerHTML = eachGuesses[n-1].result[1];
 }
 
-// initialization
-// btn onclick
-function newGame(){
-    document.getElementById("guessBoard").style.display = "none";
-    document.getElementById("answerBoard").style.display = "block";
-    document.getElementById("scoreBoard").style.display = "none";
-    document.getElementById("levelDisplay").innerHTML = "";
-    document.getElementById("times").innerHTML = times + 1;
-    for(i=1; i<=sum; i++){
-        document.getElementById("gameBoard").deleteRow(1);
+// 写入表格第n局游戏得分
+function tableWriteScore(eachGames){
+    const {id, n, isRepeat, kString, eachGuesses} = eachGames;
+    let newScore = document.getElementById('scoreDisplay').insertRow(id+2);
+    newScore.insertCell(0).innerHTML = id;
+    newScore.insertCell(1).innerHTML = n;
+    newScore.insertCell(2).innerHTML = isRepeat.toString();
+    newScore.insertCell(3).innerHTML = kString;
+    newScore.insertCell(4).innerHTML = eachGuesses.length;
+    // newScore.insertCell(5).innerHTML = time;
+    // newScore.insertCell(6).innerHTML = "...";
+}
+tableWriteScore(eachGames[0]);
+tableWriteScore(eachGames[1]);
+
+// 游戏输入后触发
+function guessNumberClick(){
+    const {n, kString, eachGuesses} = newGame;
+    if(document.getElementById("iString").reportValidity()){
+        const newGuess = {};
+        newGuess.iString = document.getElementById('iString').value;
+        newGuess.result = gameCaculate(n, newGuess.iString);
+        const {iString} = newGuess;
+        eachGuesses.push(newGuess);
+        tableWriteGame(eachGuesses, eachGuesses.length);
+
+        if(kString == iString){
+            document.getElementById("gameResult").style.display = "block";
+            document.getElementById("guessBoard").style.display = "none";
+            document.getElementById("guessDisplay").style.display = "none";
+            eachGames.push(newGame);
+
+            document.getElementById("scoreDisplay").style.display = "block";
+            tableWriteScore(newGame);
+        }
     }
-    document.getElementById("gameResult").innerHTML = "";
-    sum = 0;    // 计次归零
+    document.getElementById("iString").value = "";
+}
+
+function newGameClick(){
+    document.getElementById("gameResult").style.display = "none";
+    document.getElementById("scoreDisplay").style.display = "none";
+    document.getElementById("answerBoard").style.display = "block";
+    document.getElementById("guessDisplay").style.display = "none";
+    document.getElementById("guessBoard").style.display = "none";
+    document.getElementById("times").innerHTML = eachGames.length - 2;
+    document.getElementById("timesDisplay").innerHTML = eachGames.length - 2;
+    
+    const deleteRows = document.getElementById("gameDisplay").rows.length;
+    for(i=1; i<deleteRows; i++){
+        document.getElementById("gameDisplay").deleteRow(1);
+    }
+}
+
+function restartClick(){
+    const feedback = confirm("重设难度将会丢失当前进度，确认吗？");
+    if(feedback){
+        newGameClick();
+    }
+}
+
+function historyClick(){
+    if(document.getElementById("btn-history").value == "历史记录"){
+        document.getElementById("scoreDisplay").style.display = "block";
+        document.getElementById("btn-history").value = "隐藏记录";
+    } else{
+        document.getElementById("scoreDisplay").style.display = "none";
+        document.getElementById("btn-history").value = "历史记录";
+    }
+    
 }
